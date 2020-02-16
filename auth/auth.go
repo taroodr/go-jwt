5d9bd92fe90361af120c4b2b2f4e54db8e493cf5
taroodr <n.kotaro.0101@gmail.com>
2020-02-16 19:06:38 +0900

    add jwt auth

---
+++ auth/auth.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+)
+
+var GetTokenHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["admin"] = true
+	claims["sub"] = "54546557354"
+	claims["name"] = "taro"
+	claims["iat"] = time.Now()
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+
+	tokenString, _ := token.SignedString([]byte(os.Getenv("SIGNINGKEY")))
+
+	w.Write([]byte(tokenString))
+
+})
+
+var JwtMiddleware = jwtmiddleware.New(jwtmiddleware.Options{
+	ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SIGNINGKEY")), nil
+	},
+	SigningMethod: jwt.SigningMethodHS256,
+})
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/taroodr/go-jwt/auth"
 )
 
 type post struct {
@@ -18,6 +19,8 @@ func main() {
 	r := mux.NewRouter()
 
 	r.Handle("/public", public)
+	r.Handle("/private", auth.JwtMiddleware.Handler(private))
+	r.Handle("/auth", auth.GetTokenHandler)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal("ListenAndServe:", nil)
@@ -29,6 +32,15 @@ var public = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		Title: "VueCLIからVue.js入門①【VueCLIで出てくるファイルを概要図で理解】",
 		Tag:   "Vue.js",
 		URL:   "https://qiita.com/po3rin/items/3968f825f3c86f9c4e21",
+	}
+	json.NewEncoder(w).Encode(post)
+})
+
+var private = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	post := &post{
+		Title: "VGolangとGoogle Cloud Vision APIで画像から文字認識するCLIを速攻でつくる",
+		Tag:   "Go",
+		URL:   "https://qiita.com/po3rin/items/bf439424e38757c1e69b",
 	}
 	json.NewEncoder(w).Encode(post)
 })
